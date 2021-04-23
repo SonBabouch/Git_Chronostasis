c982f09fcee6d0f629a8fd53535e7e6c12663b21
Bastien <b.prigent@rubika-edu.com>
2021-04-23 16:24:25 +0200

    Add gitignore

---
+++ Chronostasis/Assets/Scripts/MyButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Button button;
+    public bool isPressed;
+    public bool isOnButton;
+
+    public  void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isOnButton = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOnButton = false;
+    }
+
+    public  void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+}
---
+++ Chronostasis/Assets/Scripts/TimelineManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineManager : MonoBehaviour
+{
+    public static TimelineManager Instance;
+    public Transform[] crans = new Transform[13];
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
