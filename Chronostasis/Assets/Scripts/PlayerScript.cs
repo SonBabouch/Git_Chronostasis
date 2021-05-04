967c3c2282121d6aeedc8427e55af1777e5b077e
Bastien <b.prigent@rubika-edu.com>
2021-05-04 15:23:53 +0200

    04/052021 serre

---
+++ Chronostasis/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public static PlayerScript Instance;
+    public Transform playerTransform;
+    public int playerCrans = 0;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        playerTransform.transform.position = TimelineManager.Instance.baseCran.transform.position;
+    }
+
+    private void Update()
+    {
+        playerMoveOnTimeline();
+    }
+
+    public void playerMoveOnTimeline()
+    {
+        switch(playerCrans)
+        {
+            case 0:
+                playerMove(playerCrans);
+                break;
+            case 1:
+                playerMove(playerCrans);
+                break;
+            case 2:
+                playerMove(playerCrans);
+                break;
+            case 3:
+                playerMove(playerCrans);
+                break;
+            case 4:
+                playerMove(playerCrans);
+                break;
+            case 5:
+                playerMove(playerCrans);
+                break;
+            case 6:
+                playerMove(playerCrans);
+                break;
+        }
+    }
+
+    public void playerMove(int index)
+    {
+        playerTransform.transform.position = TimelineManager.Instance.characterTimeline[index].transform.position;
+    }
+
+
+}
--- Chronostasis/Assets/Scripts/TimelineManager.cs
+++ Chronostasis/Assets/Scripts/TimelineManager.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class TimelineManager : MonoBehaviour
 {
     public static TimelineManager Instance;
-    public Transform[] crans = new Transform[13];
+    public Transform[] skillsTimeline = new Transform[7];
+    public Transform[] characterTimeline = new Transform[7];
+    public Transform baseCran;
+    public bool canPlayerMove;
+    public bool canMoveOnTimeline;
     private void Awake()
     {
         if (Instance == null)
@@ -19,5 +23,35 @@ public class TimelineManager : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        if (PlayerScript.Instance.playerCrans <= 0)
+        {
+            canMoveOnTimeline = false;
+        }
+        else canMoveOnTimeline = true;
 
+
+        if (canPlayerMove && canMoveOnTimeline)
+        {
+            StartCoroutine(MakePlayerMove());
+        }
+
+    }
+    public IEnumerator MakePlayerMove()
+    {
+        canPlayerMove = false;
+        yield return new WaitForSeconds(1f);
+        PlayerScript.Instance.playerCrans--;
+        canPlayerMove = true;
+    }
+
+    //public IEnumerator CombatTimer()
+    //{
+    //    takingTimeAway = true;
+    //    yield return new WaitForSeconds(1);
+    //    Enemy.Instance.health -= 0.5f;
+    //    secondsLeft -= 1;
+    //    takingTimeAway = false;
+    //}
 }
